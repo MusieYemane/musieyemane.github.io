e4cedf2c5106ae890ba2319b643815c6b9cc03cb
Musie Yemane <m80007278@gmail.com>
2021-11-07 18:17:43 -0600

    Update maze.js

--- lab7-maze/maze.js
+++ lab7-maze/maze.js
@@ -1,41 +1,43 @@
-let lost = false;
 
 $(document).ready(function () {
+  let lost = false;
 
   $("#start").click(function () {
+    $("#status").text('Click the "S" to begin');
+
     if (!lost) {
       $("div .boundary").mouseenter(
-        function () {
-          $("div .boundary").addClass("youlose");
-          $("#status").text("You Lose! :(");
-          $("#end").unbind("mouseenter");
+        function(){
+          lostAction();
           lost = true;
-        })
-
-
+        });
       $("#maze").mouseleave(
-        function () {
-          $("div .boundary").addClass("youlose");
-          $("#status").text("You Lose! :(");
-          $("#end").unbind("mouseenter");
+        function(){
+          lostAction();
           lost = true;
         });
     }
     else {
-      $("#status").text('Click the "S" to begin');
       $("div .boundary").removeClass("youlose");
       $("#end").mouseenter(function () {
-        $("#status").text("You Win! :)");
-        $("div .boundary").unbind("mouseenter");
-        $("#maze").unbind("mouseleave");
+        winAction();
+        lost = false;
+
       })
-      lost = false;
     }
 
   })
 
-
-
-
-
 });
+
+function lostAction(){
+  $("div .boundary").addClass("youlose");
+  $("#status").text("You Lose! :(");
+  $("#end").unbind("mouseenter");
+}
+
+function winAction(){
+  $("#status").text("You Win! :)");
+  $("div .boundary").unbind("mouseenter");
+  $("#maze").unbind("mouseleave");
+}
