ff38449630e9ddb9821b88d48cba37d472eafe5a
Musie Yemane <m80007278@gmail.com>
2021-11-08 17:58:44 -0600

    Update fifteen.js

--- hw9-puzzle/fifteen.js
+++ hw9-puzzle/fifteen.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-"use strict";
+//"use strict";
 $(document).ready(function () {
 
     var count = 0;//count variable for left and right positioning
