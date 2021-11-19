20adb04e3d80d0c0f0fb4df2b5adbe76c9d33b42
Musie Yemane <m80007278@gmail.com>
2021-11-19 12:22:43 -0600

    l

--- Project-dictionary/Dictionary.js
+++ Project-dictionary/Dictionary.js
@@ -3,11 +3,25 @@ var express = require('express');
 var app = express();
 var port = 2021
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+})
 
 app.listen(port, () => console.log("Server start on port: " + port))
 
